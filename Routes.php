b1cf9f46dfe717416928c92c5573bb68dc9e6ce6
jssejera <jssejera@fit.edu.ph>
2026-09-18 22:26:21 +0800

    Delete Routes.php

--- Routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use CodeIgniter\Router\RouteCollection;
-
-/** @var RouteCollection $routes */
-$routes->get('/', 'Pages::index');
-$routes->get('/about', 'Pages::about');
-$routes->get('/customers', 'Customers::index');
-$routes->get('/users', 'Users::index');
